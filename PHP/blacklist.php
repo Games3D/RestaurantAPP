d57bd793c16147c095c44cb93e5c5e094c66a0bc
S680 <36686252+S680@users.noreply.github.com>
2018-04-04 00:03:17 -0400

    Create blacklist.php

---
+++ PHP/blacklist.php
@@ -0,0 +1,56 @@
+
+<?php
+
+$user_Name = $_SESSION['username'];
+//$user_Name = 'sahar_e';
+
+$query = "SELECT * FROM Favorites WHERE USERNAME = '$user_Name' AND BLACKLIST = 'bl'";
+
+$result = $conn->query($query); 
+	
+	echo "<h2>". $user_Name ."'s Favorites </h2>";
+$counter = 1;
+
+while($row = $result -> fetch_assoc())
+{
+	if (!empty($row["FOODTYPE"] ) )
+	{
+//	echo "<br><strong>" - </strong>";
+
+echo "<strong> *Favorit Food Type : </strong>". $row["FOODTYPE"]."<br>";
+	}
+
+
+	if(!empty($row["FAVPLACES"]))
+	{	
+//	echo "<br><strong>". $counter. " - </strong>";
+
+echo "<strong> *Favorit Place: </strong>". $row["FAVPLACES"]."<br>";
+    }
+	
+	
+	if(!empty($row["FAVNAME"]))
+	{	
+//	echo "<br><strong>". $counter. " - </strong>";
+
+echo "<strong> *Place Name: </strong>". $row["FAVNAME"]."<br>";
+    }
+
+
+	$counter++;
+
+	echo "<br>";	
+
+	}
+	
+
+	
+	if($counter === 1)
+	{
+	echo "You have not added any Favorites!";
+	}
+	
+	
+	
+?>
+	
